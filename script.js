f2a1ae988b4e3cececc39f2a9783d0514ca974c2
Jeff <mrjeffrey1011@gmail.com>
2022-05-13 15:55:45 -0400

    The gameboard and cards are made, which flip 5 seconds after you click the confirm button on the instructions pop up

---
+++ script.js
@@ -0,0 +1,27 @@
+const instructions = function(){
+    confirm('RULES:You must make a match if the two cards turned picture-side-up are identical. When a match is made the cards will be displaced from the game board. If the two cards turned over are not identical YOU LOSE, after pressing OK you will have 5 seconds to memorize the cards.')
+
+}
+
+instructions()
+
+const cards = document.querySelectorAll('.cards')
+console.log(cards)
+
+// console.log(cards)
+// const flip = function(){
+//     console.log("flipped")
+// }
+
+const cardFlip = function(){
+    cards.forEach(function(card){
+        console.log("flipped")
+        card.style.backgroundColor = 'red'
+        card.innerText = ''
+    })
+}
+
+console.log(cardFlip)
+
+setTimeout(cardFlip, 5000)
+
