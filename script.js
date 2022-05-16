9121d820f931f9373506260fdee8af91f4463b0d
Jeff <mrjeffrey1011@gmail.com>
2022-05-16 12:28:28 -0400

    matching logic complete, working on adding windows 98 style modals

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 const instructions = function(){
     confirm('RULES:You must make a match if the two cards turned picture-side-up are identical. When a match is made the cards will be displaced from the game board. If the two cards turned over are not identical YOU LOSE, after pressing OK you will have 5 seconds to memorize the cards.')
-
 }
-
 instructions()
 
-const cards = document.querySelectorAll('.cards')
 
-const cardFlip = function(){
-    cards.forEach(function(card){
-        console.log("flipped")
-        card.style.backgroundColor = 'red'
-        card.style.color = 'red'
+// reveals cards to you after OK for 5 seconds
+const backImgs = document.querySelectorAll('img.back')
+
+backImgs.forEach(function(backImg){
+    document.addEventListener('DOMContentLoaded',function(){
+            backImg.style.display = 'none'
+            setTimeout(function(){
+                backImg.style.display = 'block'
+            }, 5000)
     })
-}
 
-console.log(cardFlip)
-
-setTimeout(cardFlip, 5000)
-
-
-
-
-
-const gameboard = [
-]
-
-
-cards.forEach(function(card){
-
-
-    card.addEventListener('click', function(){
-        card.value = card.innerText
-        card.style.background = 'white'
-        gameboard.push(card.value)
-        console.log(gameboard)
-        
-        if(gameboard[0]=== gameboard[1]){
-            setInterval(function(){
-                card.remove()
-            },2000)
-        }
-
-        // else if(gameboard[0] !== gameboard[1]){
-        //     gameboard.pop()
-        //     gameboard.pop()
-            
-        // }
-
-    })
 })
 
-console.log(gameboard)
+// when player clicks a card it reveals the clicked card 
+
+let isFlipped = false
+let firstCard 
+let secondCard
+
+
+
+const flipBackImg =  function(){
+    console.log('clicked')
+    this.style.display = 'none'
+    if(!isFlipped){
+        isFlipped = true
+        firstCard = this
+        console.log(firstCard.dataset.card)
+    }else{
+        isFlipped = false 
+        secondCard = this 
+        console.log(secondCard.dataset.card)
+        if(firstCard.dataset.card !== secondCard.dataset.card){
+            alert('GAMEOVER')
+        }
+    }
+
+}
+
+
+
+
+const cardsImgs = document.querySelectorAll('img.back')
+cardsImgs.forEach(function(backImg){
+    backImg.addEventListener('click', flipBackImg)
+})
+
+
