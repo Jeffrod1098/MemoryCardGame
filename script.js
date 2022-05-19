c3ead65920661d97b82c7883dc736986620ac644
Jeff <mrjeffrey1011@gmail.com>
2022-05-19 12:36:54 -0400

    start buttons for both levels complete and secondlevel is also complete, modal for end game needs to be comeplete

--- script.js
+++ script.js
@@ -5,19 +5,29 @@ const startBtn = document.querySelector('#startBtn')
 
 // const cardsImgs = document.querySelectorAll('img.back')
 
+let startBtnClick = 0
 
-backImgs.forEach(function(backImg){
-    startBtn.addEventListener('click',function(){
-            backImgs.forEach(function(backImg){
-                backImg.addEventListener('click', flipBackImg)
-            })
-            backImg.style.display = 'none'
-            setTimeout(function(){
-                backImg.style.display = 'block'
-            }, 2000)
+const startBtnFunc = function(){
+    if(startBtnClick === 0){
+        backImgs.forEach(function(backImg){
+                    backImgs.forEach(function(backImg){
+                        backImg.addEventListener('click', flipBackImg)
+                    })
+                    backImg.style.display = 'none'
+                    setTimeout(function(){
+                        backImg.style.display = 'block'
+                    }, 4000)
+    
+
     })
+    }
+    startBtnClick ++
+    startBtn.removeEventListener('click', startBtnFunc)
+}
 
-})
+    startBtn.addEventListener('click', startBtnFunc)
+
+
 
 
 // setting up start button
@@ -30,6 +40,8 @@ const gameOverModal = document.querySelector('#overModal')
 const restartBtn = document.querySelector('#restart')
 const nextLevelModal = document.querySelector('#nxtLvl')
 const nextLevelBtn = document.querySelector('#nextBtn')
+const endModal = document.querySelector('#endgame')
+const endBtn = document.querySelector('#congrats')
 
 instructionBtn.onclick = function(){
     modal.style.display = 'block'
@@ -43,7 +55,9 @@ restartBtn.onclick = function(){
     window.location.reload()
 }
 
-
+endBtn.onclick = function(){
+    window.location.reload()
+}
 // when player clicks a card it reveals the clicked card 
 
 let isFlipped = false
@@ -64,11 +78,12 @@ const flipBackImg =  function(){
         secondCard = this 
         console.log(secondCard.dataset.card)
         if(firstCard.dataset.card !== secondCard.dataset.card){
-            // alert('GAMEOVER')
             gameOverModal.style.display = 'block'
-        } else if(clicks >= 6){
+        } else if(clicks === 6){
             console.log('YOU WIN')
             nextLevelModal.style.display = 'block'
+        } else if(clicks === -1){
+                endModal.style.display = 'block'
         }
     }
 
@@ -76,7 +91,6 @@ const flipBackImg =  function(){
 
 
 // This radomizes the order in which the cards appear.
-// https://javascript.tutorialink.com/javascript-shuffle-html-list-element-order/
 const gameBoard = document.querySelector('.gameboard')
 for (let i = gameBoard.childElementCount; i >= 0; i--){
     gameBoard.appendChild(gameBoard.children[Math.random() * i | 0])
@@ -86,12 +100,9 @@ for (let i = gameBoard.childElementCount; i >= 0; i--){
 
 // this will add append new cards to the game and cover the cards again so that the sequence starts over again
 nextLevelBtn.onclick = function(){
+    startBtnClick = 0
+    clicks = -9 
     nextLevelModal.style.display = 'none'
-
-    const btn = document.createElement('button')
-    btn.id = "#levelTwoBtn"
-    // document.querySelector('btnContainer').appendChild(btn)
-
     for(let i = 0; i <= 1; i++ ){
         const newDiv = document.createElement('div')
         newDiv.classList.add("cards")
@@ -120,32 +131,17 @@ nextLevelBtn.onclick = function(){
     newDiv.appendChild(divBackImg)
     gameBoard.appendChild(newDiv)
 
-    setTimeout(function(){
-        divBackImg.style.display = 'block'
-    }, .1)
+    // setTimeout(function(){
+    //     divBackImg.style.display = 'block'
+    // }, .00001)
 
     const backImgs = document.querySelectorAll('img.back')
-    const cardsImgs = document.querySelectorAll('img.back')
+    // const cardsImgs = document.querySelectorAll('img.back')
 
-    backImgs.forEach(function(backImg){
-        cardsImgs.forEach(function(backImg){
-            backImg.addEventListener('click', flipBackImg)
-        backImg.style.display = 'none'
-        setTimeout(function(){
-            backImg.style.display = 'block'
-        }, .1)
-
-        startBtn.addEventListener('click',function(){
-            backImgs.forEach(function(backImg){
-                backImg.addEventListener('click', flipBackImg)
-            })
-            backImg.style.display = 'none'
-            setTimeout(function(){
-                backImg.style.display = 'block'
-            }, 4000)
-    })
-})
-})
+    // After next level, reselect all divs with queryselector, run startBtnFunc using the same variable but that variable includes the past divs and new divs
+    // You can run as many levels as you want.
+    // Also at the end of startBtnFunc you want to remove eventclicker from start so that it doesn't duplicate
+    // 
 
     gameBoard.id = "levelTwoGameBoard"
     const divCard = document.querySelectorAll('div.cards')
@@ -153,5 +149,35 @@ nextLevelBtn.onclick = function(){
     const btnContainer = document.querySelector('.btnContainer')
     btnContainer.id= 'btnContainerTwo'
     const h1 = document.querySelector('h1')
-    // h1.classList.add('')
+
+   backImgs.forEach(function(backImg){
+        backImg.addEventListener('click', flipBackImg)
+        backImg.style.display = 'none'
+        setTimeout(function(){
+            backImg.style.display = 'block'
+        }, .000001)
+    })
+
+    function showCards () {
+        backImgs.forEach(function(backImg){
+            backImg.style.display = 'none'
+            setTimeout(function(){
+                backImg.style.display = 'block'
+            }, 4000)
+        })
+        startBtn.removeEventListener('click', showCards);
+        }
+
+    startBtn.addEventListener('click', showCards) 
+
+        const lvlTwoGameBoard = document.querySelector('#levelTwoGameBoard')
+        console.log(lvlTwoGameBoard)
+        for (let i = lvlTwoGameBoard.childElementCount; i >= 0; i--){
+            lvlTwoGameBoard.appendChild(lvlTwoGameBoard.children[Math.random() * i | 0])
+        }
+
 }
+
+
+
+
